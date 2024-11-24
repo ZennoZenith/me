a35de9730e851a29eaa48cc22149c8ec1e02c737
ZennoZenith <ZennoZenith@proton.me>
2024-11-24 17:18:03 +0530

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,14 @@
-import { mdsvex } from "mdsvex";
-// import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-node';
-import staticAdapter from "@sveltejs/adapter-static";
+// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
+// import staticAdapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess(), mdsvex()],
   kit: {
-    adapter: staticAdapter({
-      pages: "build",
-      assets: "build",
-      fallback: undefined,
-      precompress: true,
-      strict: true,
-    }),
+    adapter: adapter(),
   },
   extensions: [".svelte", ".svx"],
   compilerOptions: {
